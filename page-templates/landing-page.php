9f1ae4f8a197c92ce670eb2c032b72d1c71c88a3
Andrew Adcock <aadcock@unleashed-technologies.com>
2018-12-16 10:43:52 -0500

    Adjustment to Landing Page template
    Adjust how template items (blocks) are called.

--- page-templates/landing-page.php
+++ page-templates/landing-page.php
@@ -12,24 +12,20 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 get_header();
-$container = get_theme_mod( 'understrap_container_type' );
+$container = 'landing-page';
 ?>
 
 <div class="wrapper" id="full-width-page-wrapper">
 
     <div class="<?php echo esc_attr( $container ); ?>" id="content">
 
-        <div class="row">
+<!--        <div class="row">-->
 
-            <div class="col-md-12 content-area" id="primary">
+            <div class="content-area" id="primary-landing">
 
-                <main class="site-main" id="main" role="main">
+                    <?php
+                    while ( have_posts() ) : the_post();
 
-                    <?php while ( have_posts() ) : the_post(); ?>
-
-                        <?php get_template_part( 'loop-templates/content', 'page' ); ?>
-
-                        <?php
                         if( have_rows('wyble-blocks') ):
 
                             // loop through the rows of data
@@ -39,27 +35,14 @@ $container = get_theme_mod( 'understrap_container_type' );
                                 get_template_part( 'block-templates/block', get_row_layout() );
 
                             endwhile;
-                            else: echo "Nope";
 
                         endif;
-                        ?>
 
-                        <?php
-                        // If comments are open or we have at least one comment, load up the comment template.
-                        if ( comments_open() || get_comments_number() ) :
-
-                            comments_template();
-
-                        endif;
-                        ?>
-
-                    <?php endwhile; // end of the loop. ?>
-
-                </main><!-- #main -->
+                    endwhile; // end of the loop. ?>
 
             </div><!-- #primary -->
 
-        </div><!-- .row end -->
+        <!-- </div> --><!-- .row end -->
 
     </div><!-- Container end -->
 
