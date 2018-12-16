e71271a3af2ae5ce998ac618a375a6681f057295
Andrew Adcock <aadcock@unleashed-technologies.com>
2018-12-16 14:23:31 -0500

    Responsive styles for CTA Block Parallax

--- block-templates/block-cta.php
+++ block-templates/block-cta.php
@@ -21,6 +21,9 @@ $subtitle = get_sub_field('subtitle');
 $use_overlay = get_sub_field('use_overlay');
 $overlay_color = get_sub_field('overlay_color');
 $overlay_opacity = get_sub_field('overlay_opacity');
+$show_quotes = get_sub_field('show_quotes');
+$quote1 = get_sub_field('quote_1');
+$quote2 = get_sub_field('quote_2');
 
 $cta_class = ($type == 'std' ? 'std' : 'parallax');
 $article_classes = 'block cta cta-' . $cta_class;
@@ -58,33 +61,39 @@ endif;
     </article>
 <?php elseif('parallax' == $cta_class): ?>
     <article <?php post_class($article_classes); ?> style="background-image: url('<?php echo $image['url']; ?>');">
-        <div class="entry-content">
             <?php if($use_overlay) : ?>
                 <div class="container-overlay" style="<?php echo $overlay; ?>">
             <?php endif; ?>
             <div class="container">
-                <div class="row">
+                <div class="cta-content">
                 <?php if( isset($title) || isset($subtitle)) : ?>
                     <div class="cta-header">
                         <h3><?php echo isset($title) ? $title : '' ?></h3>
                         <h4><?php echo isset($subtitle) ? $subtitle : '' ?></h4>
                     </div>
                 <?php endif; ?>
-                <?php if (isset($copy) ) : ?>
-                    <div class="cta-copy">
-                        <?php echo $copy; ?>
-                    </div>
+                <?php if(!$show_quotes) : ?>
+                    <?php if (isset($copy)) : ?>
+                        <div class="cta-copy">
+                            <?php echo $copy; ?>
+                        </div>
+                    <?php endif; ?>
+                <?php else: ?>
+                    <div class="cta-quotes">
+                        <div class="quote-1"><?php echo $quote1; ?></div>
+                        <div class="quote-2"><?php echo $quote2; ?></div>
+                    </div><!-- .cta-quotes -->
                 <?php endif; ?>
                 <?php if (isset($button_location) && isset($button_copy)) : ?>
                     <div class="cta-button">
                         <a class="btn btn-secondary" role="button" href="<?php the_permalink($button_location->ID); ?>"><?php echo $button_copy; ?></a>
                     </div><!-- .cta-button -->
                 <?php endif; ?>
-                </div><!-- .row -->
+                </div><!-- .cta-content -->
+
             </div><!-- .container -->
             <?php if($use_overlay): ?>
                 </div> <!-- /container-overlay -->
             <?php endif; ?>
-        </div><!-- .entry-content -->
     </article><!-- .block-cta-## -->
 <?php endif; ?>
