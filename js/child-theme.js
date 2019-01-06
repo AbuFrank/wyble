bf01823e8e9d5e14be51c5e48395abf2bf99782d
Andrew Adcock <aadcock@unleashed-technologies.com>
2019-01-06 09:51:09 -0500

    Add Owl Carousel function calls

--- js/child-theme.js
+++ js/child-theme.js
@@ -3976,3 +3976,17 @@
 		}, false );
 	}
 })();
+
+jQuery(document).ready(function($){
+    $('.owl-carousel').owlCarousel({
+        nav: false,
+        items: 1,
+        dots: false,
+        loop: true,
+        thumbs: true,
+        thumbImage: false,
+        thumbsPrerendered: true,
+        thumbContainerClass: 'owl-thumbs',
+        thumbItemClass: 'owl-thumb-item'
+    });
+});
